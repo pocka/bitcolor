e2aea78fd03ad22c501915fb5c4eef2bdd3f1bf7
Shota Fuji <pockawoooh@gmail.com>
2019-10-04 09:16:11 +0000

    feat: Add hex/a string converter

--- src/converter/index.ts
+++ src/converter/index.ts
@@ -16,3 +16,35 @@ export const toRgbString = (v: Color): string => {
 
   return `rgb(${r},${g},${b})`
 }
+
+const pad = (str: string) => ('00' + str).slice(-2)
+
+/**
+ * Generate #rrggbbaa style string.
+ *
+ * @param v - color to stringify
+ * @returns hexa string
+ */
+export const toHexaString = (v: Color): string => {
+  const r = getR(v).toString(16)
+  const g = getG(v).toString(16)
+  const b = getB(v).toString(16)
+  const a = getA(v).toString(16)
+
+  return `#${pad(r)}${pad(g)}${pad(b)}${pad(a)}`
+}
+
+/**
+ * Generate #rrggbb style string.
+ * Alpha channel will be ignored.
+ *
+ * @param v - color to stringify
+ * @returns hex string
+ */
+export const toHexString = (v: Color): string => {
+  const r = getR(v).toString(16)
+  const g = getG(v).toString(16)
+  const b = getB(v).toString(16)
+
+  return `#${pad(r)}${pad(g)}${pad(b)}`
+}
