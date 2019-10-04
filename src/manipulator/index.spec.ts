1b5dbe5c9c4cc1479d50296d930bf7188bc6c13a
Shota Fuji <pockawoooh@gmail.com>
2019-10-04 09:16:28 +0000

    feat: Add blend function

--- src/manipulator/index.spec.ts
+++ src/manipulator/index.spec.ts
@@ -1,5 +1,5 @@
-import { setAlpha } from '.'
-import { fromRgba, toRgbaString } from '../'
+import { setAlpha, blend } from '.'
+import { fromRgba, fromHexString, toRgbaString, toHexString } from '../'
 
 describe('#setAlpha', () => {
   const color = fromRgba(1, 2, 3, 1)
@@ -10,8 +10,32 @@ describe('#setAlpha', () => {
     expect(toRgbaString(setAlpha(color, 1))).toEqual('rgba(1,2,3,1)')
   })
 
-  it('Should limit value between 0 and 1', () => {
+  it('Should clip alpha between 0 and 1', () => {
     expect(toRgbaString(setAlpha(color, -5))).toEqual('rgba(1,2,3,0)')
     expect(toRgbaString(setAlpha(color, 255))).toEqual('rgba(1,2,3,1)')
+  })
+})
+
+describe('#blend', () => {
+  const color1 = fromHexString('#036')
+  const color2 = fromHexString('#d2e1dd')
+
+  it('Should blend two colors', () => {
+    // These tests are taken from Sass's docs
+    expect(toHexString(blend(color1, color2))).toEqual('#698aa2')
+    expect(toHexString(blend(color1, color2, 0.75))).toEqual('#355f84')
+    expect(toHexString(blend(color1, color2, 0.25))).toEqual('#9eb6bf')
+    expect(
+      toRgbaString(
+        blend(fromRgba(242, 236, 228, 0.5), fromHexString('#6b717f'))
+      )
+    ).toEqual('rgba(141,144,152,0.75)')
+  })
+
+  it('Should clip weight between 0 and 1', () => {
+    expect(toHexString(blend(color1, color2, 100))).toEqual(toHexString(color1))
+    expect(toHexString(blend(color1, color2, -100))).toEqual(
+      toHexString(color2)
+    )
   })
 })
--- src/manipulator/index.ts
+++ src/manipulator/index.ts
@@ -1,4 +1,14 @@
-import { Color, A_MASK } from '../shared'
+import {
+  Color,
+  A_MASK,
+  R_OFFSET,
+  G_OFFSET,
+  B_OFFSET,
+  getA,
+  getB,
+  getG,
+  getR
+} from '../shared'
 
 /**
  * Set alpha to the given value.
@@ -9,3 +19,40 @@ import { Color, A_MASK } from '../shared'
  */
 export const setAlpha = (color: Color, alpha: number): Color =>
   (color & ~A_MASK) | (Math.min(Math.max(alpha, 0), 1) * 255)
+
+/**
+ * Blend two colors.
+ * Ratio is calculated based on weight and each colors' alpha value.
+ *
+ * @param a - the base color
+ * @param b - the added color
+ * @param weight - how much of a is in the result, between 0.0-1.0
+ * @returns the blended color
+ */
+export const blend = (a: Color, b: Color, weight: number = 0.5): Color => {
+  const clippedWeight = Math.min(1, Math.max(0, weight))
+
+  const alphaA = getA(a)
+  const alphaB = getA(b)
+
+  // This mixing algorithm is ported from Sass.
+  // https://github.com/sass/dart-sass/blob/1.23.0/lib/src/functions/color.dart#L720
+  const normalizedWeight = clippedWeight * 2 - 1
+  const alphaDistance = alphaA / 255 - alphaB / 255
+
+  const combinedWeight =
+    normalizedWeight * alphaDistance === -1
+      ? normalizedWeight
+      : (normalizedWeight + alphaDistance) /
+        (1 + normalizedWeight * alphaDistance)
+
+  // Final weight of a, clipped to 0.0-1.0
+  const w = (combinedWeight + 1) / 2
+
+  return (
+    (Math.round(getR(a) * w + getR(b) * (1 - w)) << R_OFFSET) |
+    (Math.round(getG(a) * w + getG(b) * (1 - w)) << G_OFFSET) |
+    (Math.round(getB(a) * w + getB(b) * (1 - w)) << B_OFFSET) |
+    (Math.round(alphaA * clippedWeight + alphaB * (1 - clippedWeight)) | 0)
+  )
+}
