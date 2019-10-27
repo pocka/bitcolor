fec1dd649a1ea94b8b94d29677d22453f7896081
Shota Fuji <pockawoooh@gmail.com>
2019-10-27 06:02:09 +0000

    refactor: Refactor bit utilities
    To prepare to the future changing internal data representation or
    addition of other data types.

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,13 +1,17 @@
 export type Color = number
 
-export const R_OFFSET = 8 * 3
-export const G_OFFSET = 8 * 2
-export const B_OFFSET = 8
+const RGBA_BIT_SIZE = 8
 
-export const R_MASK = 0xff << R_OFFSET
-export const G_MASK = 0xff << G_OFFSET
-export const B_MASK = 0xff << B_OFFSET
-export const A_MASK = 0xff
+export const R_OFFSET = RGBA_BIT_SIZE * 3
+export const G_OFFSET = RGBA_BIT_SIZE * 2
+export const B_OFFSET = RGBA_BIT_SIZE
+
+const RGBA_MASK_VALUE = (1 << RGBA_BIT_SIZE) - 1
+
+export const R_MASK = RGBA_MASK_VALUE << R_OFFSET
+export const G_MASK = RGBA_MASK_VALUE << G_OFFSET
+export const B_MASK = RGBA_MASK_VALUE << B_OFFSET
+export const A_MASK = RGBA_MASK_VALUE
 
 export const getR = (v: number) => (v & R_MASK) >>> R_OFFSET
 export const getG = (v: number) => (v & G_MASK) >>> G_OFFSET
